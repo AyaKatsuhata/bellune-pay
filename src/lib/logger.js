7733f658c46717fa1d9c589061854a94df4667d7
akatsuhata <aya.katsuhata@elife.co.jp>
2025-09-04 22:49:22 +0900

    commit

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -4,7 +4,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SER
 
 export async function logger({ level, lineId, message, context }) {
     const context = typeof context === 'object' ? JSON.stringify(context, Object.getOwnPropertyNames(context)) : String(context)
-
+    const lineId = ''
     const { error } = await supabase
     .from('logs')
     .insert([{ level, lineId, message, context }]);
