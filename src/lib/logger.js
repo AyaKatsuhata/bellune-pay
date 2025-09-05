5552128f1bb245497852c6fcb80f976beec873cc
akatsuhata <aya.katsuhata@elife.co.jp>
2025-09-06 00:17:22 +0900

    logger fix

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
-export async function logger({ level, lineId, message, contextStr }) {
-    const context = typeof contextStr === 'object' ? JSON.stringify(contextStr, Object.getOwnPropertyNames(contextStr)) : String(contextStr)
+export async function logger({ level, lineId, message, context }) {
+    // const context = typeof contextStr === 'object' ? JSON.stringify(contextStr, Object.getOwnPropertyNames(contextStr)) : String(contextStr)
     const { error } = await supabase
     .from('logs')
     .insert([{ level, lineId, message, context }]);
